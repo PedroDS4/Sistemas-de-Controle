f6458a20ebbec482e02e7ce11cccbf034c189a61
Pedro <105248310+PedroDS4@users.noreply.github.com>
2026-06-13 19:05:27 -0300

    Add files via upload

---
+++ Sistemas_de_Controle_DCA3701/Unidade3/Space_State_Observator.m
@@ -0,0 +1,158 @@
+% Código para solucionar problemas referentes a realimentação de Estados
+
+dt = 5e-3;
+
+t_max = 10;
+
+N = t_max/dt;
+
+A = [[-4 3 0];[-1 -4 -1];[0 0 -2]];
+B = [0 0 3]';
+C = [1 0 0];
+
+
+U = controlability_matrix(A, B);
+
+V = observability_matrix(A, C);
+
+p = [ -1+1i -1-1i -10];
+
+L = state_observator(A, C, p);
+
+
+A_error = A - L*C;
+
+x = zeros(3, N);
+x_hat = zeros(3, N);
+e = zeros(3, N);
+x(:, 1) = [1 1 1];
+
+e = x - x_hat;
+
+u = ones(1, N);
+
+
+% Space State Simulation
+
+for i = 1:N-1
+    x(:, i+1) = x(:, i) + dt*(A*x(:, i) + B*u(i));
+    y(i+1) = C*x(:, i+1);
+    e(:, i+1) = e(:, i) + dt*(A_error*e(:, i));
+end
+
+x_hat = x - e;
+
+
+t = linspace(0, t_max, N);
+blue = [0, 0.5, 0.8];
+red = [0.4, 0.1, 0];
+yellow = [1, 0.1, 0.5];
+
+
+color = {blue yellow red};
+color2 = {yellow, red, blue};
+
+
+for i = 1:3
+    figure()
+    plot(t, x(i, :), 'color', color{i}, 'linewidth', 4 + 2*(4 - i)); hold on;
+    plot(t, x_hat(i, :), 'color', color2{i}, 'linewidth', 1 + (4 - i));
+    legend('x', 'x_{est}');
+    xlabel('Time(s)');
+    ylabel('Amplitude');
+    grid on
+end
+
+
+y_hat = C*x_hat;
+
+figure()
+plot(t, y, 'color', 'green', 'linewidth', 4 ); hold on
+plot(t, y_hat, 'color', 'black', 'linewidth', 2 );
+legend('y', 'y_{est}');
+xlabel('Time(s)');
+ylabel('Amplitude');
+grid on
+
+
+
+
+
+
+
+
+
+function U = controlability_matrix(A, B)
+
+    N = length(A);
+    
+    U = zeros([N, N]);
+    
+    for i = 1:N
+        U(:, i) = ((A^(i-1))*B);
+    end
+    
+    
+end
+
+
+
+function V = observability_matrix(A, C)
+
+    N = length(A);
+    
+    V = zeros([N, N]);
+    
+    for i = 1:N
+        V(i, :) = C*(A^(i-1));
+    end
+    
+    
+end
+    
+    
+function q_c = delta_s(A, p_s)
+
+    N = length(A);
+    q_c = zeros([N, N]);
+    
+    for i = 1:N+1
+        q_c = q_c + p_s(i)*A^(N+1-i);
+    end
+    
+end
+
+
+
+
+        
+function K = state_realimentation(A, B, p)
+    
+    U = controlability_matrix(A, B);
+    N = length(A);
+    p_s = poly(diag(p));
+    q_c = delta_s(A, p_s);
+    
+    v_aux = zeros([1, N]);
+    v_aux(end) = 1;
+    
+    K = -v_aux * inv(U) * q_c;
+
+end
+
+
+
+function L = state_observator(A, C, p)
+    
+    V = observability_matrix(A, C);
+    N = length(A);
+    p_s = poly(diag(p));
+    q_o = delta_s(A, p_s);
+    
+    v_aux = zeros([1, N])';
+    v_aux(end) = 1;
+    
+    L = q_o * inv(V) * v_aux;
+
+end
+
